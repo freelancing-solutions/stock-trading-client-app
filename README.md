9aebc9be5b6b3c5818f772cfec9c00acf65b17aa
freelancing-solutions <mobiusndou@gmail.com>
2021-05-21 21:36:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mtxwkx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mtxwkx)
---
+++ src/components/Home/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Pinoy Desk</h1>
+    </div>
+  );
+}

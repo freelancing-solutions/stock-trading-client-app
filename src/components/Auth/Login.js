223e535650673628d32e6cd7d61c844305d397cd
mobius-crypt <mobiusndou@gmail.com>
2021-05-22 23:28:03 +0200

    Updated

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,24 +1,13 @@
 import React from "react"
-import * as init from "../../contexts/init"
-import firebase from "firebase/app"
-import * as firebaseui from "firebaseui"
+import "firebase/auth"
+const firebaseui = require("firebaseui")
+import {auth,uiConfig} from "../../contexts/init"
 import 'firebaseui/dist/firebaseui.css'
 
 export default function Login(){
-    const firebaseApp = firebase.apps[0]
-    const uiConfig = {
-        signInSuccessUrl: '/dashboard',
-        signInOptions: [
-            firebaseApp.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebaseApp.auth.EmailAuthProvider.PROVIDER_ID,
-            firebaseApp.auth.AnonymousAuthProvider.PROVIDER_ID
-        ],
-        tosUrl: '/terms',
-        privacyPolicyUrl: function() {
-            window.location.assign('/privacy');
-        }
-    }
-    let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    const ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
+    ui.start('firebaseui-auth-container', uiConfig)
     return (
         <div id='firebaseui-auth-container'> </div>
     )
--- src/contexts/init.js
+++ src/contexts/init.js
@@ -6,10 +6,13 @@
  *
  */
 import React from "react"
-import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import "firebase/storage"
+import firebase from "firebase"
 
 // noinspection SpellCheckingInspection
-firebase.initializeApp({
+const config = {
     apiKey: "AIzaSyC0EXV8zXvovXt4XnsqJ6F1qck9k-zVUJo",
     authDomain: "pinoydesk.firebaseapp.com",
     projectId: "pinoydesk",
@@ -17,5 +20,32 @@ firebase.initializeApp({
     messagingSenderId: "521703065329",
     appId: "1:521703065329:web:a4f955b9f2d4e0e3464b50",
     measurementId: "G-HZNRYF3L51"
-})
+}
 
+try{
+    if (!firebase.apps.length){
+        firebase.initializeApp(config);
+    }else{
+        firebase.app();
+    }
+}catch (e) {
+    console.log('firebase app already configured');
+}
+
+const uiConfig = {
+    signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    tosUrl: '/terms',
+    signInSuccessUrl: '/dashboard',
+    privacyPolicyUrl: function() {
+        window.location.assign('/privacy');
+    }
+}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const storage = firebase.storage();
+export {auth,firestore,storage, uiConfig};

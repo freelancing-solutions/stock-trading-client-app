b2eeccd19bed447d536731b4c4c788787a11fe08
mobius-crypt <mobiusndou@gmail.com>
2021-05-28 19:10:47 +0200

    up

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import ReactDOM from 'react-dom';
 import App from './components/Home/App.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+//TODO open the following web apps,
+/***
+ * FontFlipper
+ * cloudcraft for budgeting
+ * insomnia
+ * flare
+ * figma
+ */
